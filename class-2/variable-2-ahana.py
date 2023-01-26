5170dcd06fdcdeda4aafe3d9f654ad0ba9cd32db
Ahana28 <123613385+Ahana28@users.noreply.github.com>
2023-01-26 20:02:53 +1100

    christmas
    made for fun

--- class-2/variable-2-ahana.py
+++ class-2/variable-2-ahana.py
@@ -1,4 +1,4 @@
-# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume)
+
 myvar = "John"     # A variable name must start with a letter or the underscore character
 my_var = "John"
 _my_var = "John"
@@ -6,14 +6,14 @@ myVar = "John"    # Variable names are case-sensitive (age, Age and AGE are thre
 MYVAR = "John"
 myvar2 = "John"   # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 
-2myvar = "John"      # A variable name cannot start with a number
-my-var = "John"
-my var = "John"
 
 # Each word, except the first, starts with a capital letter
-myVariableName = "John"    # Camel Case
+myVariableName = "John"  
+
 # Each word starts with a capital letter
 MyVariableName = "John"    # Pascal Case
+
+
 # Each word is separated by an underscore character
 my_variable_name = "John"
 
--- class-2/variable-3-ahana.py
+++ class-2/variable-3-ahana.py
@@ -1,6 +1,6 @@
 # Many Values to Multiple Variables
 x, y, z = "Orange", "Banana", "Cherry"
-print(x)
+print(x) 
 print(y)
 print(z)
 # One Value to Multiple Variables
