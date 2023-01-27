fe56851967eadfa3c2ae74d6ec7d0f231b1e916a
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-01-27 15:03:38 +1100

    puzzle for everyone in progress
    have fun

---
+++ class-2/puzzle-achyut.py
@@ -0,0 +1,24 @@
+#find where the errors are.
+
+print(
+hello world
+)
+
+if 3 < 4:
+print("Hi")
+
+if 3 < 4 
+    print("hello")
+    
+3my= "why"
+
+@chyut = 23
+
+
+print("  /\  )
+print(" /  \")
+
+
+
+
+
