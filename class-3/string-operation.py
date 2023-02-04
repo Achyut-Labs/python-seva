1ea521d4af4d5a7329e7302c5df87d094f295e80
TheRochero <123613643+TheRochero@users.noreply.github.com>
2023-02-04 17:47:36 +1100

    Update string-operation.py

--- class-3/string-operation.py
+++ class-3/string-operation.py
@@ -1,8 +1,36 @@
+<<<<<<< Updated upstream
 x = "my first day of school was great"
 
 
 
+=======
+<<<<<<< HEAD
+x = "my first day of school was good"
+print(x[0])
+print(x[16:22])
+
+print(len(x))
+
+for y in x:
+    print(y)
+print(y)
+for y in x:
+
+x= ['monday', 'tuesday', 'wednesday']
+for y in x:
+    print(y)
+print(y)
+=======
+x = "my first day of school was great"
+
+
+
+>>>>>>> Stashed changes
 print(x[16:22])
 
 for y in x:
     print("")
+<<<<<<< Updated upstream
+=======
+>>>>>>> f1e1dfaaddd59f483cb06e59fdbd4793bda4d75e
+>>>>>>> Stashed changes
