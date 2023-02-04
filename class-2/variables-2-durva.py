9d15fef042902fb55e4cca45f013c7ac9ff78cf4
thesweetnspicyariesdurva13 <pateldurva13@gmail.com>
2023-02-04 17:43:27 +1100

    Create variables-2-durva.py

---
+++ class-2/variables-2-durva.py
@@ -0,0 +1,18 @@
+# A varaable can have a short name (like x and y) or a more descriptive name (age, car name, total_volume).
+myvar = "John"    # Varaiable names must start with a letter or the underscore character.
+my_var = "John"
+_my_var = "John"
+myVar = "John"    # Variables names are case-sensitive (age, Age and AGE are three differnt variables)
+MYVAR = "John"
+myvar2 = "John"   # A variable name can only contain alpha-numeric characters and underscore (A-z, 0-9, and _)
+
+2myvar = "John"   # A variable name cannot start with a number
+my-var = "John"
+my var = "John"
+
+# Each word, except the first, starts with a capital letter
+myVariableName = "John"   # Camel Case
+# Each word stars with a capital letter
+MyVariableName = "John"   # Pascal Case
+# Each word is separated with an underscore character
+my_variable_name = "John"
