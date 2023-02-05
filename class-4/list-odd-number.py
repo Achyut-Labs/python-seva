820b96513b7395dc5eee268fab7bc7b9e83c9b54
Jayesh PATEL <info@achyutlabs.com>
2023-02-05 23:47:53 +1100

    add example of list

--- class-4/list-odd-number.py
+++ class-4/list-odd-number.py
@@ -3,6 +3,16 @@ a = [10,3,14,15,17]
 b = []
 
 print(len(a))
+for i in a:
+    if i % 2 != 0:
+        b.append(i)
+
+print(b)
+
+# Print elements from odd positions in a list
+a = [10,3,14,15,17]
+b = []
+
 for i in range(0, len(a)):
     if i % 2 != 0:
         b.append(a[i])
