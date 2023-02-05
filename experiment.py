381f65c97b5382f01b02a785f2009aad48cc88a5
codingkrish42 <123613636+codingkrish42@users.noreply.github.com>
2023-02-05 16:26:30 +1100

    Variables

--- experiment.py
+++ experiment.py
@@ -1,1 +1,7 @@
 print("i like strawberries")
+print("i don't like bananas")
+print("i love cherries")
+
+s,b,c= strawberries, bananas, cherries
+
+print('s')
---
+++ variablesbykrisha.py
@@ -0,0 +1,8 @@
+x=float(3)   # this will be 3.0
+y=str(3)     # this will be "3"
+z=int(3)     # this will be 3
+
+print(x,y,z)
+
+
+
