0a7e8d030eb5c00c0c18e1c5bee417e5468b2a5b
codingkrish42 <123613636+codingkrish42@users.noreply.github.com>
2023-02-11 09:39:54 +1100

    Lists-Krisha.py
    Explanation of lists in my own words.

---
+++ class-3/lists-krisha.py
@@ -0,0 +1,24 @@
+thislist = ("apple", "banana", "cherry", "apple", "cherry")  #this is a list. It is ordered, changeable
+print(thislist)                                              #and can have duplicates 
+
+
+thislist = ("apple", "banana", "cherry")                     #this code will tell us how many items are in 
+print(len(thislist))                                         #this list (len())
+
+
+list1 = ("apple", "banana", "cherry")    #string
+list2 = (1,5,7,9,3)                      #integer 
+list3 = (True, False, True)              #booleen            #booleen is when the answer can be true or false
+
+
+list1 = ("abc", 34, True, 40, "male")    #some lists can have booleen, string and integer values   
+
+
+mylist = ("apple", "banana", "cherry")
+print(type(mylist))                      #this code will help to know what type of list a list is 
+
+
+#List is a collection which is ordered and changeable. Can have duplicate members.
+#Tuple is a collection which is ordered and unchangeable. Can have duplicate members.
+#Set is a collection which is unordered, unchangeable, and unindexed. No duplicate members.
+#Dictionary is a collection which is ordered and changeable. No duplicate members.
