e1bc891a0f35229de0bb782b46f45a93be7038ab
thesweetnspicyariesdurva13 <pateldurva13@gmail.com>
2023-02-18 19:33:38 +1100

    hbvhb

--- class-5/odd numbers durva.py
+++ class-5/odd numbers durva.py
@@ -1,9 +1,17 @@
 x=input("Enter the odd number:")
 x=int(x)
+<<<<<<< Updated upstream
 i =0
 result=[]
 for i in range(0,x):
     if i %2 !=0:
         result.append(i)
+=======
+1=0
+result-[]
+for i in rnge(0,x):
+    if i %2 !==0
+     result.append(i)
+>>>>>>> Stashed changes
 
 print(result)
