c3bfebe4a769c84a426b9a3a2892fd24be22026d
thesweetnspicyariesdurva13 <pateldurva13@gmail.com>
2023-02-18 19:51:34 +1100

    Update odd numbers durva.py

--- class-5/odd numbers durva.py
+++ class-5/odd numbers durva.py
@@ -1,17 +1,13 @@
-x=input("Enter the odd number:")
+x=input("Enter a number:")
 x=int(x)
-<<<<<<< Updated upstream
+
 i =0
+
 result=[]
 for i in range(0,x):
     if i %2 !=0:
         result.append(i)
-=======
-1=0
-result-[]
-for i in rnge(0,x):
-    if i %2 !==0
-     result.append(i)
->>>>>>> Stashed changes
+
+
 
 print(result)
