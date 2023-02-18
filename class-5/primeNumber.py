c7ba0eb6fea1b588967f01d95315c72974d425f2
moi-san <mauryapat27@gmail.com>
2023-02-18 19:29:15 +1100

    Update primeNumber.py

--- class-5/primeNumber.py
+++ class-5/primeNumber.py
@@ -1,8 +1,8 @@
-x = 50
-i = 0, result = [ ]
-for i in range(x):
- if i % 2 != 0
- result uppend (i)
-prime (result)
-
-
+x= (20)
+x=int(x)
+i=0
+result= []
+for i in range (0,x):
+    if i %2 !=0:
+        result.append(i)
+print(result)
