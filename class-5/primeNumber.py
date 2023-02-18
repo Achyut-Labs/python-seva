3f54086d0cab4d355b6ad358c53633c713cedb7a
Ahana28 <123613385+Ahana28@users.noreply.github.com>
2023-02-18 19:53:57 +1100

    ?

--- class-5/primeNumber.py
+++ class-5/primeNumber.py
@@ -1,35 +1,26 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 x = input("Enter the number to find all prime numbers less than that number:")
-
-
-
-
-a=[]
-b=[]
-c=[]
-d=[]
-e=[]
-f=[]
-g=[]
-h=[]
-j=[]
-x=0
-y=50
-for x in range:(y)
 =======
-x= (20)
-x=int(x)
-i=0
-result= []
-for i in range (0,x):
-    if i %2 !=0:
+x = input("evennumber:")
+x = int(x)
+i = 0
+result = []
+>>>>>>> Stashed changes
+
+for i in range(i,x):
+    if i % 2 ==0:
         result.append(i)
+
 print(result)
+<<<<<<< Updated upstream
 >>>>>>> 9f5b0b45477596152b8c12b56130d0ea4608882e
 =======
 
 
 a= 0
 for x in 0 :range(x)
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
