cf6eee5f8d9587dda4b92971540fc561d853e428
thesweetnspicyariesdurva13 <pateldurva13@gmail.com>
2023-02-26 17:16:01 +1100

    Create twolitintoone durva.py

---
+++ excercises/twolitintoone durva.py
@@ -0,0 +1,18 @@
+
+sun = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+rain = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+rainbow = []
+
+
+print(len(sun))
+for i in sun:
+    if i %2  != 0:
+        rainbow.append (i)
+
+
+print(len(rain))
+for i in rain:
+    if i %2  == 0:
+        rainbow.append (i)
+
+print(rainbow)  
