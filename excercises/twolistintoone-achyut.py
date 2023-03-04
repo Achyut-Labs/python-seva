9b9381f79ca5292038d1b3a7ab3f9cebef26586c
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-03-04 14:40:33 +1100

    dfd
    dfd

--- excercises/twolistintoone-achyut.py
+++ excercises/twolistintoone-achyut.py
@@ -1,18 +1,18 @@
-hi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-bye = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-helloAgain = []
+cricket = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+cricket2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+helloAgainCricket = []
 
 
 #odd
-for i in hi:
+for i in cricket:
     if i % 2 != 0:
-        helloAgain.append (i)
+        helloAgainCricket.append (i)
 
 
 
 #even
-for i in bye:
+for i in cricket2:
     if i % 2 == 0:
-        helloAgain.append (i)
+        helloAgainCricket.append (i)
 
-print(helloAgain)  
+print(helloAgainCricket)  
