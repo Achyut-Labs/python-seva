411e0e95643b8f2215a1c5dd7b717e68d442ac5a
moi-san <mauryapat27@gmail.com>
2023-03-04 15:18:07 +1100

    dgbfs

--- excercises/primenumber.py
+++ excercises/primenumber.py
@@ -2,7 +2,7 @@
 num = int(input("Enter a number: "))
 
 # define a flag variable
-flag = False
+#flag = False
 
 if num == 1:
     print(num, "is not a prime number")
--- excercises/stringspaceremoval.py
+++ excercises/stringspaceremoval.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Take sentences as input and print each character one by one however remove the space if any in sentence.
 
 x = "my first day of school was good"
@@ -9,3 +10,12 @@ for y in x:
 
 
     
+=======
+# Take sentences as input and print each character one by one however remove the space if any in sentence
+myString = "I like food because it is good"
+
+for char in myString:
+    if char == " ":
+        continue
+    print(char)
+>>>>>>> Stashed changes
