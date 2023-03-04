a9a192a97637919d1168248fededb6870f96f24a
Achyut Labs <info@achyutlabs.com>
2023-03-04 15:10:52 +1100

    renamed Emma to Durva

--- excercises/countsubstrings.py
+++ excercises/countsubstrings.py
@@ -1,3 +1,3 @@
 # Return the count of a given substring from a string
-# Write a program to find how many times substring “Emma” appears in the given string
-str_x = "Emma is good developer. Emma is a writer"
+# Write a program to find how many times substring “Durva” appears in the given string
+str_x = "Durva is good developer. Durva is a writer"
