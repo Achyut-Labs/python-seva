c08fd72dc7a6d952bd53bdfc8b90aa577692073c
Ahana28 <123613385+Ahana28@users.noreply.github.com>
2023-03-04 14:05:40 +1100

    .

--- excercises/stringspaceremoval.py
+++ excercises/stringspaceremoval.py
@@ -1,5 +1,6 @@
 # Take sentences as input and print each character one by one however remove the space if any in sentence.
 
+<<<<<<< Updated upstream
 x = "my first day of school was good"
 
 for y in x:
@@ -9,3 +10,11 @@ for y in x:
 
 
     
+=======
+myString = "Pasta is yummy"
+
+for char in myString:
+    if char == " ":
+      continue
+print(char)
+>>>>>>> Stashed changes
