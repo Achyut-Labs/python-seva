075cd81b66044b86776692e973246c03a7f8fbed
LimbachiyaDhruv <dhruv10.limbachiya@gmail.com>
2023-03-04 15:24:05 +1100

    Update countsubstrings.py

--- excercises/countsubstrings.py
+++ excercises/countsubstrings.py
@@ -1,3 +1,3 @@
 # Return the count of a given substring from a string
-# Write a program to find how many times substring “Durva” appears in the given string
+# Write a program to find how many times substring “Durva”   appears in the given string
 str_x = "Durva is good developer. Durva is a writer"
