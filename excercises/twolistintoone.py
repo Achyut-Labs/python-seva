bc55224bc9ca0b617f6ca81c1778a150dce43344
moi-san <mauryapat27@gmail.com>
2023-03-04 15:24:29 +1100

    dgbfs

--- excercises/twolistintoone.py
+++ excercises/twolistintoone.py
@@ -1,22 +1,4 @@
 # Crate a new list from a two list using the following condition
 # Create a new list from a two list using the following condition
-food=[1,4,6,9,7]
-water=[2,5,8,3,0]
-eggs=[]
-
-print(len(food))
-for i in food:
-    if i % 2 != 0:
-        eggs.append(i)
-
-print(eggs)
-
-print(len(water))
-for i in water:
-    if i % 2 == 0:
-        eggs.append(i)
-
-print(eggs)
-
-
-
+# Given a two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+# the new list should contain odd numbers from the first list and even numbers from the second list.===========================================================================================================================================================================================================================
