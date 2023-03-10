e8aa32dc2d42265f373e13d033f6c255ca0ad46a
Jayesh PATEL <info@achyutlabs.com>
2023-03-11 01:52:54 +1100

    new excercise

---
+++ excercise1/testmybasic.py
@@ -0,0 +1,12 @@
+# Write a program to check qualities of characters and match the qualities
+
+characters = ["ram", "krishna", "hanuman", "ganesh", "sita", "shiva", "brahma", "vishnu"]
+
+negative_emotion = ["angry", "afraid", "anxious", "confused", "depressed", "destroyed", "disappointed", "disgusted", "embarrassed", "stupid"]
+positive_emotion = ["happy", "attractive", "awake", "comfortable", "confident", "content", "curious", "excited", "peaceful", "pleasant"]
+
+
+
+
+
+
