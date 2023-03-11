b4a7fbf44733d00b7f3ca669a90352bdc04ba002
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-03-11 16:47:54 +1100

    f

---
+++ class-7/excercises/Ternery-achyut.py
@@ -0,0 +1,15 @@
+emotion = input('Enter your emotion, angry or happy: ')
+
+goodOrBadState = 'You are in a bad state.' if str(emotion) == 'angry' else 'You are in a great state'
+
+print(f'{goodOrBadState} good for you')
+
+
+
+# Task 2
+
+Char = input('enter a your character, Ram or Krishna:  ')
+
+Quality = 'Ram is a complete human being he is Moderation' if Char == 'Ram' else 'Krishna is a perfect complete human being he has no limits'
+
+print(Quality)
