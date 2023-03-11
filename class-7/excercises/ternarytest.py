ba9b9e3b6d39bb225e5d48ec5483ea58208e21a6
Ahana28 <123613385+Ahana28@users.noreply.github.com>
2023-03-11 16:07:07 +1100

    Work

--- class-7/excercises/ternarytest.py
+++ class-7/excercises/ternarytest.py
@@ -1,5 +1,5 @@
-age = input('50:')
+age = input('Enter your age:')
 
-ticket_price = 20 if int(50) >= 18 else 5
+ticket_price = 20 if int(age) >= 18 else 5
 
 print(f"The ticket price is {ticket_price}")
