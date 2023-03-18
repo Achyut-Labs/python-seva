f3f4d7662c8bb085fb17e3c513bb1cf62569e599
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-03-18 13:46:52 +1100

    Update Achyut-count-digit.py

--- excercise1/Achyut-count-digit.py
+++ excercise1/Achyut-count-digit.py
@@ -1,12 +1,6 @@
 # this input sentence
 x = input("write a number: ")
-
-
-# Changing the inputed number into a list to separate everything. 
-x = list(x)
-
 # printing the amount of digits
-
 print("The amount of characters in the number are",len(x))
 
 
