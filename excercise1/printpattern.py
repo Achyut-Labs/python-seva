344dfb5702cc52ed637131e77d5e8991af30b1bd
TheRochero <123613643+TheRochero@users.noreply.github.com>
2023-03-18 13:36:06 +1100

    print pattern!  FINALLY

--- excercise1/printpattern.py
+++ excercise1/printpattern.py
@@ -1,5 +1,5 @@
 '''
-Write a program to use for loop to print the following reverse number pattern
+Write a program to use for loop to print the following reverse number pattern 
 
 5 4 3 2 1
 4 3 2 1
@@ -8,3 +8,27 @@ Write a program to use for loop to print the following reverse number pattern
 1
 
 '''
+a = ['5 4 3 2 1']
+
+for b in a:
+    print(b)
+
+    b = ['4 3 2 1']
+
+    for c in b:
+        print(c)
+
+        c=['3 2 1']
+
+        for d in c:
+            print(d)
+
+            d = ['2 1']
+
+            for e in d:
+                print(e)
+
+                e = ['1']
+
+                for f in e:
+                    print(f)
