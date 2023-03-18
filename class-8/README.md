a31a3f94d1d05eae246f0fe99d55278db8ffce1f
Achyut Labs <info@achyutlabs.com>
2023-03-18 14:58:13 +1100

    function

--- class-8/README.md
+++ class-8/README.md
@@ -1,7 +1,7 @@
 # Ojectives of this lession - Class - 8
 # Functions
 
-```py
+```
 def printme():
     """ Display a greeting to users """
     print('Hi')
--- class-8/additionFunction.py
+++ class-8/additionFunction.py
@@ -1,6 +1,29 @@
 def sum(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
 
-total = sum(10,20)
-print(total)
+def division(a, b):
+    return a / b
+
+def minus(a, b):
+    return a - b
+
+
+a = int(input("Enter a first number:  "))
+b = int(input("Enter a second number: "))
+
+result = sum(a, b)
+print("sum: ", result)
+
+result = multiply(a, b)
+print("Multiplication: ", result)
+
+
+result = division(a, b)
+print("division: ", result)
+
+
+result = minus(a, b)
+print("minus: ", result)
