814486ca2316571f63e6d72da042b42c9ea1200a
Achyut Labs <info@achyutlabs.com>
2023-03-18 13:39:45 +1100

    my pattern

--- excercise1/printpattern.py
+++ excercise1/printpattern.py
@@ -32,3 +32,13 @@ for b in a:
 
                 for f in e:
                     print(f)
+
+
+'''
+n = 5
+k = 5
+for i in range(0,n+1):
+    for j in range(k-i,0,-1):
+        print(j,end=' ')
+    print()
+'''                    
