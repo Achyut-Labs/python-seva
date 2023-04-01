944b0c414c97229d1f0f4e31b5b9666871bf432e
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-04-01 15:44:22 +1100

    f
    fg

--- AchyutdictionariesExcersices/Excersice1.py
+++ AchyutdictionariesExcersices/Excersice1.py
@@ -18,3 +18,6 @@ values = [10, 20, 30]
 
 dict = dict(zip(keys, values))
 print(dict)
+
+
+#found answer using https://careerkarma.com/blog/python-convert-list-to-dictionary/#:~:text=To%20convert%20a%20list%20to%20a%20dictionary%20using%20the%20same,the%20values%20of%20a%20list.
--- AchyutdictionariesExcersices/Excersice2.py
+++ AchyutdictionariesExcersices/Excersice2.py
@@ -14,3 +14,5 @@ dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
 z= {**dict1,**dict2}
 print(z)
+
+#found answer using https://favtutor.com/blogs/merge-dictionaries-python#:~:text=The%20simplest%20way%20to%20concatenate,collection%20of%20key%2Dvalue%20pairs.
