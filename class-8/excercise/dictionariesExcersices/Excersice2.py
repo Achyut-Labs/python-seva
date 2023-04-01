720a7c25df4431fb723afc3dc89db1316ec4c5f0
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-04-01 15:02:41 +1100

    dfb
    dfg

---
+++ class-8/excercise/dictionariesExcersices/Excersice2.py
@@ -0,0 +1,9 @@
+'''
+Exercise 2: Merge two Python dictionaries into one
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+Expected output:
+
+{'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+'''
