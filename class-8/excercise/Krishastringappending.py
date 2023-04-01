d8ad30250025dd76553c32f58d6fb85ae3259f98
codingkrish42 <123613636+codingkrish42@users.noreply.github.com>
2023-04-01 15:06:02 +1100

    Create Krishastringappending.py
    This code combines (adds) my strings together.

---
+++ class-8/excercise/Krishastringappending.py
@@ -0,0 +1,11 @@
+a = ("ayooooo")                       #a is equal to the string "ayooooo"
+b = (" this is my function")          #b is equal to the string, " this is my function"
+ 
+def appendString(a,b):                #def means to define. append means to add/combine and in this case, it means to 
+    return a+b                        #combine a and b. 
+
+z=appendString(a,b)                   #z equals to combined string of a and b. 
+
+print(z)                              #the answer 
+
+
