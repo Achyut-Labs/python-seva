7648590d7f020f8159903763c241811df9fc37d0
patelachyut <64836212+patelachyut@users.noreply.github.com>
2023-04-01 15:29:40 +1100

    hgj
    jhjh

--- AchyutdictionariesExcersices/Excersice2.py
+++ AchyutdictionariesExcersices/Excersice2.py
@@ -7,3 +7,10 @@ Expected output:
 {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
 '''
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+
+z= {**dict1,**dict2}
+print(z)
