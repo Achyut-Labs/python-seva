05f4677f8949f88b0e735265c6e86fadd58e693a
LimbachiyaDhruv <dhruv10.limbachiya@gmail.com>
2023-04-22 15:10:14 +1000

    Create Dhurv Mad lips Generator.py

---
+++ excercise1/Dhurv Mad lips Generator.py
@@ -0,0 +1,25 @@
+noun=input("enter a noun ")
+pluralNoun=input("enter a pural noun")
+noun2=input("enter anouther noun")
+place=input("name a place ")
+adjective = input('Choose an adjective (Describing word): ')
+
+noun3 = input('Choose a noun: ')
+
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print('Be kind to your', noun, '- footed', pluralNoun)
+
+print('For a duck may be somebody\'s', noun2, ',')
+
+print('Be kind to your', pluralNoun, 'in', place)
+
+print('Where the weather is always', adjective, '. \n')
+
+print('You may think that is this the', noun3, ',')
+
+print('Well it is.')
+
+print('------------------------------------------')
