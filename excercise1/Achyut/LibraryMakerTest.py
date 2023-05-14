bb3e21ccf5dbf69b5229069d1b54df30586d7a49
Jayesh PATEL <info@achyutlabs.com>
2023-05-14 22:02:47 +1000

    Library game modified

--- excercise1/Achyut/LibraryMakerTest.py
+++ excercise1/Achyut/LibraryMakerTest.py
@@ -1,34 +1,33 @@
 import myMaths
+def recall():
+    t = True
+    while t:
+        a = input('Do you want to use the calculator again (Enter [y]es [n]o): ')
+
+        if a == 'y' or a == 'Y':
+            main()
+        elif a == 'n' or a == 'N':
+            t = False
+        else:
+            print('please inserst a valid value y or n')
+
 
 def main():
-    x = True
-    while x:
-       print('The Calculator.')
-       c = int(input('enter you number for maths: '))
-       d = int(input('enter you second number for maths: '))
-    
-       print('Addition: ',myMaths.myAddition(c,d) )
-       print( 'Multiplication:', myMaths.myMultiplication(c,d))
-       print('Division:', myMaths.myDivision(c,d))
-       print('Sunbtraction:', myMaths.mySubtraction(c,d))
-       a = input('Do you want to use the calculator again (Enter [y]es [n]o): ')
-       if a == 'y' or 'Y':
-           continue
-       elif a == 'n' or 'N':
-           x= False
-       else:
-           print('please inserst a valid value y or n')
-        
-           
-           
+    print('The Calculator.')
+    c = int(input('enter you number for maths: '))
+    d = int(input('enter you second number for maths: '))
+
+    print('Addition: ',myMaths.myAddition(c,d) )
+    print( 'Multiplication:', myMaths.myMultiplication(c,d))
+    print('Division:', myMaths.myDivision(c,d))
+    print('Sunbtraction:', myMaths.mySubtraction(c,d))
+
+    recall()
 
 
-    
-
-    
 
 if __name__ == '__main__':
     main()
-    
+
 
 
