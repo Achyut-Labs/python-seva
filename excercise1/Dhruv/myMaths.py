75a5ecd8c7da06e8e508a04aebea2db28a64f8ad
LimbachiyaDhruv <dhruv10.limbachiya@gmail.com>
2023-05-20 14:15:40 +1000

    Math game and importing

---
+++ excercise1/Dhruv/myMaths.py
@@ -0,0 +1,13 @@
+def myAddittion(a,b):
+    return a+b
+def mymultipilycation (a,b):
+    return a*b 
+def mySubtraction (a,b):
+    return a-b
+def myDivision(a,b):
+    return a/b
+
+if __name__=='__main__':
+    pass
+
+
