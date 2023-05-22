c864f668530f8405d8cfb29f415096c509da902e
thesweetnspicyariesdurva13 <pateldurva13@gmail.com>
2023-05-22 20:20:03 +1000

    Update libraryImport.py

--- excercise1/Durva/libraryImport.py
+++ excercise1/Durva/libraryImport.py
@@ -2,9 +2,21 @@ import myMaths
  
 input('Do you want to play a maths game? yes or no: ')
 
+def input():
+    t=True
+    while t:
+     d= input('Do you want to play a game? Enter y/n: ')
+     if d == yes.lower():
+        main()
+
+     elif d == no.lower():
+        t=False
+    else:
+       print(' Please enter [y]es or [n]o')
+
 
 def main():
-    print("We are doing Mathamatics")
+    print('My Pal Cal')
     a = int(input("Enter your first number for maths: "))
     b = int(input("Enter your second number for maths: "))
 
@@ -12,10 +24,6 @@ def main():
     print(myMaths.myMulipilcation(a, b))
     print(myMaths.mySubstraction(a, b))
     print(myMaths.myDivision(a, b))
-
-
-
-
 
 if __name__ == '__main__':
     main()
