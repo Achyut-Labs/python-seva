7f9e600729e2bd41e2e94fe14b03ed8ebec52b3b
moi-san <mauryapat27@gmail.com>
2023-05-27 14:07:53 +1000

    Create libaryImporter.py

---
+++ excercise1/Maurya/libaryImporter.py
@@ -0,0 +1,30 @@
+import myMaths 
+
+def calscStatus():
+    while True:
+        playagain = input("Would you wish to use the calculator again(valid responses yes or no)?")
+        if playagain == 'yes':
+            main()
+        elif playagain == 'no':
+            break
+        else:
+            print("I don't know what you are saying, please use the valid responses")
+    
+
+
+def main():
+    print("Let's do some Maths together!!")
+    a= int(input("Enter your first number"))
+    b= int(input("Enter your second number"))
+
+    print(myMaths.myAddition(a, b))
+    print(myMaths.myMultiplication(a, b))
+    print(myMaths.mySubtraction(a, b))
+    print(myMaths.myDivision(a, b)) 
+    calscStatus()
+
+
+    
+    
+if __name__ == '__main__':
+    main()
