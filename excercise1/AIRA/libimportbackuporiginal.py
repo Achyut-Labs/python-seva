23b2de746516f78aa92f4035228ed95085dd33ec
TheRochero <123613643+TheRochero@users.noreply.github.com>
2023-05-27 15:28:11 +1000

    i did something d - - b

--- excercise1/AIRA/libimportbackuporiginal.py
+++ excercise1/AIRA/libimportbackuporiginal.py
@@ -1,14 +1,14 @@
-import myMaths
+import Mymaths
 
 def main(): #refer to line 16 #Ok so we created a a library (libImport), and now we are trying to use a function but it is not being called upon so the first time to computer goes through this code, it ignore the def main(). Ok Now refer to line 16...again...
     print("We are doing Mathematics")
     a = int(input("Enter your first number for maths"))
     b = int(input("Enter your second number for maths"))
 
-    print(myMaths.myAddition(a, b))
-    print(myMaths.mySubtraction(a, b))
-    print(myMaths.myMultiplication(a, b))
-    print(myMaths.myDivision(a, b))
+    print(Mymaths.myAddition(a, b))
+    print(Mymaths.mySubtraction(a, b))
+    print(Mymaths.myMultiplication(a, b))
+    print(Mymaths.myDivision(a, b))
 
 if __name__ == '__main__':
     main() # Ok now refer to line 3
