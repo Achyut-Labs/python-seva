b233424604a7877af69809ab72b4e7ede9e35a69
LimbachiyaDhruv <dhruv10.limbachiya@gmail.com>
2023-06-23 13:43:05 +1000

    Dhurv

--- excercise1/Dhruv/myMaths.py
+++ excercise1/Dhruv/myMaths.py
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 def myAddittion(a,b):
     return a+b
 def mymultipilycation (a,b):
@@ -11,3 +15,22 @@ if __name__=='__main__':
     pass
 
 
+<<<<<<< Updated upstream
+=======
+=======
+def myAddition(a,b):
+    return a + b
+
+def myMultiplication(a,b):
+    return a * b
+
+def mySubtraction(a,b):
+    return a - b
+
+def myDivision(a,b):
+    return a / b
+
+if __name__ == '__main__':
+    pass
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
