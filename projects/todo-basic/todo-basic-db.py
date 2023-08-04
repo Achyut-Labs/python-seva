8ae54862b0e56a326343a296af2dcf9a4f404116
Jayesh PATEL <info@achyutlabs.com>
2023-08-05 02:15:16 +1000

    updated mysql db code

--- projects/todo-basic/todo-basic-db.py
+++ projects/todo-basic/todo-basic-db.py
@@ -15,6 +15,9 @@ https://dev.mysql.com/downloads/mysql/
 MySQL client
 https://dbeaver.io/download/
 
+activate virtual env before going further
+
+pip install -r requirement.txt
 '''
 
 import mysql.connector
@@ -22,29 +25,44 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="root",
+  password="root",
+  database="todo"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW DATABASES")
-mycursor.execute("CREATE DATABASE todo")
-mycursor.execute("CREATE TABLE customers (name VARCHAR(255), task VARCHAR(255))")
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
 
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Python Homework")
-mycursor.execute(sql, val)
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
 
-mycursor.commit()
+mycursor.executemany(sql, val)
 
-print(mycursor.rowcount, "record inserted.")
+mydb.commit()
+
+print(mycursor.rowcount, "was inserted.")
 
 mycursor.execute("SELECT * FROM customers")
 
-myresult = mycursor.fetchone()
+myresult = mycursor.fetchall()
 
-print(myresult)
-
-
-mycursor.close()
+for x in myresult:
+  print(x)
