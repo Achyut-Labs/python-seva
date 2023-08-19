118ba02b67fb758fc6c2e1a8ad3b56854ce87bd7
TheRochero <123613643+TheRochero@users.noreply.github.com>
2023-08-19 14:57:43 +1000

    I SAID WHAT I SAID

--- excercise4/learnbasicagain.py
+++ excercise4/learnbasicagain.py
@@ -1,5 +1,6 @@
 '''
-0. Write a Python program to find out what version of Python you are using.
+0. 
+Write a Python program to find out what version of Python you are using.
 
 1. Write a Python program that accepts a filename from the user and prints the extension of the file.
 Sample filename : abc.java
