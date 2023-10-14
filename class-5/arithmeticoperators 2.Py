b3ecafdd462d51aa1167763260bba9340ebfaeb5
Ahana28 <123613385+Ahana28@users.noreply.github.com>
2023-10-14 14:16:10 +1100

    changes

---
+++ class-5/arithmeticoperators 2.Py
@@ -0,0 +1,34 @@
+#Addition
+x =4
+y =6
+print(x+y)
+
+#Substraction
+a= 5
+b= 4
+print(a-b)
+
+#Mulitplication
+c= 3
+d= 8
+print(c*d)
+
+#Division
+e= 9
+f= 2
+print(9/2)
+
+#Modulus(remainder when the two numbers are divided)
+g= 5
+h= 2
+print(g%h)
+
+#Exponentiation(power of)
+i= 4
+j= 3
+print(4**3)
+
+#Floor Division(rounds the result down to the nearest whole number)
+k= 16
+l= 3
+print(k//l)
---
+++ dictioneriesExampleByAchyut 2.py
@@ -0,0 +1,17 @@
+racePlacement= {
+    "Winner": "Achyut",
+    "Second": "Shrey",
+    "third": "Adhya",
+    "Last": "Rudra"
+}
+
+carRegistration = {
+    "Year" : 2017,
+    "Model" : "Toyota Kluger",
+    "Number Plate" : "YNG 683"
+}
+
+
+
+
+
